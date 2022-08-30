fecdbe70eefba99f64fb2a5e54f0b7b790c9ca59
Marat <roulinwhite@gmail.com>
2022-08-30 11:59:10 +0300

    Add main page, pages, links

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,7 +7,20 @@ class Topic(models.Model):
     text = models.CharField(max_length=260)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return self.text
 
-def __str__(self):
-    """Возвращает строковое представление модели."""
-    return self.text
+
+class Entry(models.Model):
+    """Информация, изученная пользователем по теме"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return f"{self.text[:50]}..."
