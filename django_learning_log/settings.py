5019a96728721ce3847a4753d5d80ce9785e6eb5
Marat <roulinwhite@gmail.com>
2022-09-04 15:04:50 +0300

    Add topic, add entry, users, log in, log out, register pages.

--- django_learning_log/settings.py
+++ django_learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Мои приложения
     'learning_logs',
+    'users',
     # Приложения джанго по умолчанию.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Мои настройки
+LOGIN_URL = '/users/login/'
